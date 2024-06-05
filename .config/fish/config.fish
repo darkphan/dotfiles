610c13c40b1916879371ba5bf772e1ab85aba2a1
darkphan <darkphan@gmail.com>
2024-06-05 06:37:16 -0500

    minor fish config tweak

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 if test -f /usr/share/cachyos-fish-config/cachyos-config.fish
     source /usr/share/cachyos-fish-config/cachyos-config.fish
-else if test -f $HOME/.config/fish/cachyos-stuff/cachyos-config.fish
-    source $HOME/.config/fish/cachyos-stuff/cachyos-config.fish
+    #else if test -f $HOME/.config/fish/cachyos-stuff/cachyos-config.fish
+    #source $HOME/.config/fish/cachyos-stuff/cachyos-config.fish
 end
 
 # overwrite greeting
