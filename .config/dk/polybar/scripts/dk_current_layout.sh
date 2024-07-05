bcc22f9d9d5a425048aba39e8122bab4636ab044
darkphan <darkphan@gmail.com>
2024-07-05 05:59:12 -0500

    updates to core configs

--- .config/dk/polybar/scripts/dk_current_layout.sh
+++ .config/dk/polybar/scripts/dk_current_layout.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-dkcmd status num=1 type=layout | jq -r '.layout'
+MSG="$(dkcmd status num=1 type=layout | jq -r '.layout' | tr [:lower:] [:upper:])"
+ICON="dialog-information"
+notify-send -t 2000 -i "$ICON" "CURRENT LAYOUT: $MSG"
